eb35154dbd9772e4db2075e0f5696711c190ea6f
ashishgitpersonal <61772015+ashishgitpersonal@users.noreply.github.com>
2020-03-04 12:22:44 +0530

    create jave login file

---
+++ login.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class FileDemo {
+public static void main(String[] args) {
+try {
+File file = new File("javaFile123.txt");
+if (file.createNewFile()) {
+System.out.println("New File is created!" );
+} else {
